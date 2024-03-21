52bf986312693a388bd7a7519c9ed7f004f8ac41
Hudaa Eka Saputra <hudaaputra72@gmail.com>
2024-03-21 16:13:27 +0700

    contact page

--- src/app/(main)/contact/page.tsx
+++ src/app/(main)/contact/page.tsx
@@ -9,46 +9,6 @@ import {
 import { motion } from "framer-motion";
 
 export default function Contact() {
-  const contact = {
-    rest: (i: number) =>
-      i == 2
-        ? {
-            translateY: "-35px",
-          }
-        : {
-            translateY: "35px",
-          },
-    hover: {
-      translateY: "0px",
-      transition: {
-        duration: 0.4,
-      },
-    },
-  };
-
-  const hoverBackdrop = {
-    rest: {
-      height: "0",
-    },
-    hover: {
-      height: "100%",
-      transition: {
-        duration: 0.4,
-      },
-    },
-  };
-
-  const text = {
-    rest: {
-      opacity: 0,
-    },
-    hover: {
-      opacity: 1,
-      transition: {
-        duration: 0.4,
-      },
-    },
-  };
   return (
     <section>
       <div className="contact-wrapper absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 items-center justify-center gap-1 md:gap-10">
@@ -62,7 +22,7 @@ export default function Contact() {
           href="https://www.github.com/bijaklaut"
           target="_blank"
         >
-          <motion.div className="border-lightgray absolute left-1/2 top-1/2 h-60 w-16 -translate-x-1/2 -translate-y-1/2 -skew-y-[20deg] rounded-md border md:h-72 md:w-20">
+          <motion.div className="absolute left-1/2 top-1/2 h-60 w-16 -translate-x-1/2 -translate-y-1/2 -skew-y-[20deg] rounded-md border border-lightgray md:h-72 md:w-20">
             <motion.div
               variants={hoverBackdrop}
               className="absolute left-0 right-0 top-0 w-full rounded-md bg-white"
@@ -70,7 +30,7 @@ export default function Contact() {
           </motion.div>
           <motion.p
             variants={text}
-            className="text-primary absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 -rotate-90 text-2xl font-semibold md:text-3xl"
+            className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 -rotate-90 text-2xl font-semibold text-primary md:text-3xl"
           >
             Github
           </motion.p>
@@ -87,7 +47,7 @@ export default function Contact() {
           href="https://wa.me/62085920051232"
           target="_blank"
         >
-          <motion.div className="border-lightgray absolute left-1/2 top-1/2 h-60 w-16 -translate-x-1/2 -translate-y-1/2 -skew-y-[20deg] rounded-md border md:h-72 md:w-20">
+          <motion.div className="absolute left-1/2 top-1/2 h-60 w-16 -translate-x-1/2 -translate-y-1/2 -skew-y-[20deg] rounded-md border border-lightgray md:h-72 md:w-20">
             <motion.div
               variants={hoverBackdrop}
               className="absolute bottom-0 left-0 right-0 w-full rounded-md bg-white"
@@ -95,7 +55,7 @@ export default function Contact() {
           </motion.div>
           <motion.p
             variants={text}
-            className="text-primary absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 -rotate-90 text-2xl font-semibold md:text-3xl"
+            className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 -rotate-90 text-2xl font-semibold text-primary md:text-3xl"
           >
             Whatsapp
           </motion.p>
@@ -112,7 +72,7 @@ export default function Contact() {
           href="mailto:hudaaputra72@gmail.com"
           target="_blank"
         >
-          <motion.div className="border-lightgray absolute left-1/2 top-1/2 h-60 w-16 -translate-x-1/2 -translate-y-1/2 -skew-y-[20deg] rounded-md border md:h-72 md:w-20">
+          <motion.div className="absolute left-1/2 top-1/2 h-60 w-16 -translate-x-1/2 -translate-y-1/2 -skew-y-[20deg] rounded-md border border-lightgray md:h-72 md:w-20">
             <motion.div
               variants={hoverBackdrop}
               className="absolute left-0 right-0 top-0 w-full rounded-md bg-white"
@@ -120,7 +80,7 @@ export default function Contact() {
           </motion.div>
           <motion.p
             variants={text}
-            className="text-primary absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 -rotate-90 text-2xl font-semibold md:text-3xl"
+            className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 -rotate-90 text-2xl font-semibold text-primary md:text-3xl"
           >
             Email
           </motion.p>
@@ -137,7 +97,7 @@ export default function Contact() {
           href="https://www.instagram.com/hudaaptr"
           target="_blank"
         >
-          <motion.div className="border-lightgray absolute left-1/2 top-1/2 h-60 w-16 -translate-x-1/2 -translate-y-1/2 -skew-y-[20deg] rounded-md border md:h-72 md:w-20">
+          <motion.div className="absolute left-1/2 top-1/2 h-60 w-16 -translate-x-1/2 -translate-y-1/2 -skew-y-[20deg] rounded-md border border-lightgray md:h-72 md:w-20">
             <motion.div
               variants={hoverBackdrop}
               className="absolute bottom-0 left-0 right-0 w-full rounded-md bg-white"
@@ -145,7 +105,7 @@ export default function Contact() {
           </motion.div>
           <motion.p
             variants={text}
-            className="text-primary absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 -rotate-90 text-2xl font-semibold md:text-3xl"
+            className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 -rotate-90 text-2xl font-semibold text-primary md:text-3xl"
           >
             Instagram
           </motion.p>
@@ -155,3 +115,44 @@ export default function Contact() {
     </section>
   );
 }
+
+const contact = {
+  rest: (i: number) =>
+    i == 2
+      ? {
+          translateY: "-35px",
+        }
+      : {
+          translateY: "35px",
+        },
+  hover: {
+    translateY: "0px",
+    transition: {
+      duration: 0.4,
+    },
+  },
+};
+
+const hoverBackdrop = {
+  rest: {
+    height: "0",
+  },
+  hover: {
+    height: "100%",
+    transition: {
+      duration: 0.4,
+    },
+  },
+};
+
+const text = {
+  rest: {
+    opacity: 0,
+  },
+  hover: {
+    opacity: 1,
+    transition: {
+      duration: 0.4,
+    },
+  },
+};
