fe2cadd29343f7fd5d60395e6dcf107ad3553a03
Hudaa Eka Saputra <hudaaputra72@gmail.com>
2024-03-25 21:41:55 +0700

    fixing layout and adding animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,17 +9,21 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <section className="flex h-screen w-full items-center justify-center">
+    <section className="flex h-screen w-full items-center justify-center overflow-hidden bg-gradient-to-br from-darkprimary to-black">
       <div className="flex -translate-y-3 flex-col items-center gap-4">
         <nav
-          className={`${inter.className} mb-3 flex gap-8 text-lightgray transition-colors [&>*:hover]:text-white [&>*]:duration-300`}
+          className={`${inter.className} flex animate-fade-in gap-8 text-lightgray transition-colors [&>*:hover]:text-white [&>*]:duration-300`}
         >
           <Link href={"/projects"}>Projects</Link>
           <Link href={"/about"}>About Me</Link>
           <Link href={"/contact"}>Contact</Link>
         </nav>
-        <div className="hero text-[80px] font-bold">{`Hudaa Eka Saputra`}</div>
-        <div className="subtitle text-xl font-semibold text-lightgray">{`Frontend Web Developer`}</div>
+        <div className="h-px w-screen animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+        <h1 className="font-outline z-10 animate-title cursor-default whitespace-nowrap bg-white bg-clip-text text-[80px] font-semibold text-transparent transition-all duration-1000">
+          Hudaa Eka Saputra
+        </h1>
+        <div className="h-px w-screen animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+        <div className="subtitle animate-fade-in text-xl font-semibold text-lightgray">{`Frontend Web Developer`}</div>
       </div>
     </section>
   );
