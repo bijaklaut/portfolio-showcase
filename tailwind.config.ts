06c0be0279246f36e8bac1e94f3045ba2c7dd7f1
Hudaa Eka Saputra <hudaaputra72@gmail.com>
2024-03-25 21:28:47 +0700

    adding styles and animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,11 +9,6 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      // backgroundImage: {
-      //    "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-      //    "gradient-conic":
-      //       "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      // },
       keyframes: {
         moveltr: {
           "0%": { transform: "translateX(0)" },
@@ -23,9 +18,71 @@ const config: Config = {
           "0%, 100%": { transform: "rotate(-3deg)" },
           "50%": { transform: "rotate(3deg)" },
         },
+        title: {
+          "0%": {
+            "line-height": "0%",
+            "letter-spacing": "0.25em",
+            opacity: "0",
+          },
+          "25%": {
+            "line-height": "0%",
+            opacity: "0%",
+          },
+          "80%": {
+            opacity: "100%",
+          },
+
+          "100%": {
+            "line-height": "150%",
+            opacity: "100%",
+          },
+        },
+        "fade-left": {
+          "0%": {
+            transform: "translateX(100%)",
+            opacity: "0%",
+          },
+
+          "30%": {
+            transform: "translateX(0%)",
+            opacity: "100%",
+          },
+          "100%": {
+            opacity: "0%",
+          },
+        },
+        "fade-right": {
+          "0%": {
+            transform: "translateX(-100%)",
+            opacity: "0%",
+          },
+
+          "30%": {
+            transform: "translateX(0%)",
+            opacity: "100%",
+          },
+          "100%": {
+            opacity: "0%",
+          },
+        },
+        "fade-in": {
+          "0%": {
+            opacity: "0%",
+          },
+          "75%": {
+            opacity: "0%",
+          },
+          "100%": {
+            opacity: "100%",
+          },
+        },
       },
       animation: {
         ltr: "moveltr 5s linear infinite forwards",
+        title: "title 3s ease-in forwards",
+        "fade-left": "fade-left 3s ease-in-out forwards",
+        "fade-right": "fade-right 3s ease-in-out forwards",
+        "fade-in": "fade-in 3s ease-in-out forwards",
       },
       colors: {
         primary: "#9259E3",
