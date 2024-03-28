cec7d6a44dbf2256626207c84a45ba0cdbf4633d
Hudaa Eka Saputra <hudaaputra72@gmail.com>
2024-03-28 15:00:25 +0700

    fixing back button

--- src/components/TopNavbar.tsx
+++ src/components/TopNavbar.tsx
@@ -2,10 +2,14 @@
 
 import Link from "next/link";
 import { LongArrowLeftSvg } from "./SvgGroup";
-import { useCallback, useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { usePathname } from "next/navigation";
 
 export const TopNavbar = () => {
   const navbarRef = useRef<HTMLElement>(null);
+  const pathname = usePathname();
+  const [prevPath, setPrevPath] = useState("");
+
   const scrollHandler = useCallback(() => {
     if (navbarRef.current) {
       if (window.scrollY > 0) {
@@ -16,6 +20,17 @@ export const TopNavbar = () => {
     }
   }, [navbarRef]);
 
+  const getPrevPath = useCallback(() => {
+    const splittedPath = pathname.split("/");
+    splittedPath.pop();
+
+    if (splittedPath.join("/")) {
+      setPrevPath(splittedPath.join("/"));
+    } else {
+      setPrevPath("/");
+    }
+  }, [pathname]);
+
   useEffect(() => {
     window.addEventListener("scroll", scrollHandler);
     return () => {
@@ -23,13 +38,17 @@ export const TopNavbar = () => {
     };
   }, []);
 
+  useEffect(() => {
+    getPrevPath();
+  }, [pathname]);
+
   return (
     <nav
       ref={navbarRef}
       className="rest-navbar fixed z-10 flex h-16 w-full justify-between px-16 py-5 text-white/60 transition-all duration-300"
     >
       <div className="[&>*:hover]:text-white [&>*]:transition-all [&>*]:duration-300">
-        <Link href={"/"}>
+        <Link href={prevPath}>
           <LongArrowLeftSvg className="h-8 w-8 stroke-current" />
         </Link>
       </div>
