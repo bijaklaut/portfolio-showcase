cd090ddd2ba26576c2f97049ecbd0259deda67dd
Hudaa Eka Saputra <hudaaputra72@gmail.com>
2024-03-28 12:57:56 +0700

    projects file

---
+++ src/utils/types.ts
@@ -0,0 +1,12 @@
+export interface ProjectTypes {
+  project_name: string;
+  slug: string;
+  short_desc: string;
+  long_desc: string;
+  demo_link: string;
+  repo_link: string;
+  features: string[];
+  tech: string[];
+  images: string[];
+  creation_date: string;
+}
